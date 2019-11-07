430dbf5b9ad656ed4c3db986e05e9efa8172329d
jjgalang <jjgalang@co2061-11.ece.iastate.edu>
2019-11-07 08:54:51 -0600

    sorta figured out transactions

--- bank_server.c
+++ bank_server.c
@@ -134,21 +134,23 @@ int main(int argc, char **argv){
 			printf("ID %d\n", idCount);
 			
 			
-			struct job toAdd; //initialize a job
-			toAdd.request_ID = idCount; //provide it's request ID
+			struct job *toAdd;
+			toAdd = (struct job*) malloc(sizeof(struct job)); 
+			
+			toAdd->request_ID = idCount; //provide it's request ID
 			
 			int toInt = atoi(parts[1]); //conver the provided string to an Integer for account lookup
-			toAdd.check_acc_ID = read_account(toInt); //add the account ID to job
+			toAdd->check_acc_ID =read_account(toInt); //add the account ID to job
 			//toAdd.time_arrival = time; //FIX ------------------------------------------------------------
-			toAdd.next = NULL; 
+			toAdd->next = NULL; 
 				
 			
 			if(queueList.num_jobs == 0){
-				queueList.head = queueList.tail = &toAdd;
+				queueList.head = queueList.tail = toAdd;
 			}
 			else{
-				queueList.tail->next = &toAdd;
-				queueList.tail = &toAdd;
+				queueList.tail->next = toAdd;
+				queueList.tail = toAdd;
 				
 			}
 			
@@ -170,28 +172,88 @@ int main(int argc, char **argv){
 		
 		else if (strcmp(parts[0], "TRANS") == 0 ){
 	
-		if((index-1) % 2 != 0){
+			if((index-1) % 2 != 0){
 
-			printf("Odd number of arguments, rejecting request\n");
-		}
-
-		else{
-			printf("This'll work!\n");
-		}
-/**	
-			if(sizeA-1 % 2 != 0){
 				printf("Odd number of arguments, rejecting request\n");
-				printf("ID %d\n", sizeA);
-
 			}
 
 			else{
+				printf("ID %d\n", idCount);
+				
+				struct job *toAdd;
+				toAdd = (struct job*) malloc(sizeof(struct job)); 
+				//malloc toAdd and the transaction
+				
+				toAdd->request_ID = idCount;
+	
+			
+				if(queueList.num_jobs == 0){
+					queueList.head = queueList.tail = toAdd;
+				}
+				else{
+					queueList.tail->next = toAdd;
+					queueList.tail = toAdd;
+				
+				}
+				// 0 1 2 
+				
+				/**
+					int acc_id;  //    account id
+					int amount;  //    amount to be added, can be positive or negative
+				**/
+				
+				
+				int c, r = 0; 
+				
+				
+				/**
+				struct trans *temp[(idCount / 2)];
+				temp = (struct trans) malloc(sizeof(struct *trans)); 
+				
+				
+				temp->acc_id = 2; 
+				
+				printf("%d\n", temp[0]);
+				**/
+				
+				
+				struct trans *temp = toAdd->transactions; 
+				temp = (struct trans*) malloc(sizeof(struct trans)); 
+				
+				for(c = 2; c <= index; c += 2){
+				
+					int account = atoi(parts[c-1]);
+					int amnt = atoi(parts[c]);
+					temp[r].acc_id = account; 
+					temp[r].amount = amnt;
+					//temp += 1; 
+					r++;
+					
+				
+					printf("acc_id - %d\n", account);
+					printf("amount - %d\n", amnt);
+				
+					
+				}
+				
+				for(r = 0; r < (index-1) / 2; r++){
+				
+					printf("%d\n", temp[r].acc_id);
+					printf("%d\n", temp[r].amount);
+					
+				
+				}
+				
+				
+				
+				
+				idCount++;
+				
+				queueList.num_jobs++;
 
-				printf("ID %d\n", sizeA);
-			
-			
-				sizeA = 0; 
-**/
+			}	
+			}
+
 	
 					
 		/**
@@ -202,23 +264,7 @@ int main(int argc, char **argv){
 	struct timeval time_arrival, time_end; // arrival time and end time
 	struct job *next; // pointer to the next request**/
 			
-				struct job toAdd; 
-				toAdd.request_ID = idCount;
-			
-				if(queueList.num_jobs == 0){
-					queueList.head = queueList.tail = &toAdd;
-				}
-				else{
-					queueList.tail->next = &toAdd;
-					queueList.tail = &toAdd;
 				
-				}
-			
-				idCount++;
-				
-				queueList.num_jobs++;
-
-			}	
 		
 		//}
 		
@@ -230,6 +276,9 @@ int main(int argc, char **argv){
 	
 		
 	}
+	
+	
+	//free the malloc in the worker thread
 	
 	/**
 			logic for TRANS
